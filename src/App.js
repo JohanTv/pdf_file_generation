f9867b05cf2979e970dadb741e4c9e2dc4e617d1
JohanTv <johan.tanta@utec.edu.pe>
2022-11-17 01:38:20 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { PrimerComponente } from './components/PrimerComponente'
+import { SegundoComponente } from './components/SegundoComponente'
+import { SimpleTable } from './components/SimpleTable';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Esto es un repaso de React con Johan Tanta</p>
+        <PrimerComponente />
+        <SegundoComponente />
+        <SimpleTable />
+        {/* <button onClick={() => execute()}>Prueba</button> */}
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/ExcelTable.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { SpreadSheets, Worksheet, Column } from '@grapecity/spread-sheets-react';
+// import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
+import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2016colorful.css';
+
+// import GC from '@grapecity/spread-sheets';
+// let SpreadJSKey = "xxx";
+// GC.Spread.Sheets.LicenseKey = SpreadJSKey;
+
+class ExcelTable extends Component {
+	constructor(props) {
+		super(props);
+		this.spreadBackColor = 'aliceblue';
+		this.sheetName = 'Goods List';
+		this.hostStyle = {
+			width: '670px',
+			height: '600px'
+		};
+		this.data = [
+			{
+				Name: "Apple",
+				Category: "Fruit",
+				Price: 1,
+				"Shopping Place": "Wal-Mart",
+			},
+			{
+				Name: "Potato",
+				Category: "Fruit",
+				Price: 2.01,
+				"Shopping Place": "Other",
+			},
+			{
+				Name: "Tomato",
+				Category: "Vegetable",
+				Price: 3.21,
+				"Shopping Place": "Other",
+			},
+			{
+				Name: "Sandwich",
+				Category: "Food",
+				Price: 2,
+				"Shopping Place": "Wal-Mart",
+			},
+			{
+				Name: "Hamburger",
+				Category: "Food",
+				Price: 2,
+				"Shopping Place": "Wal-Mart",
+			},
+			{
+				Name: "Grape",
+				Category: "Fruit",
+				Price: 4,
+				"Shopping Place": "Sun Store",
+			},
+		];
+		this.columnWidth = 100;
+	}
+	render() {
+		return (
+			<div>
+				<SpreadSheets backColor={this.spreadBackColor} hostStyle={this.hostStyle}>
+					<Worksheet name={this.sheetName} dataSource={this.data}>
+						<Column dataField='Name' width={300}></Column>
+						<Column dataField='Category' width={this.columnWidth}></Column>
+						<Column dataField='Price' width={this.columnWidth} formatter="$#.00"></Column>
+						<Column dataField='Shopping Place' width={this.columnWidth}></Column>
+					</Worksheet>
+				</SpreadSheets>
+			</div>
+		)
+	}
+}
+export default ExcelTable   
