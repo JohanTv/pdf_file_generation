4f57e3747af9199e061e72797da7c70d138624b4
JohanTv <johan.tanta@utec.edu.pe>
2022-11-18 23:23:39 -0500

    start of export in gallery format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { PrimerComponente } from './components/PrimerComponente'
-import { SegundoComponente } from './components/SegundoComponente'
-import { SimpleTable } from './components/SimpleTable';
 import { PDFTable } from './components/PDFTable';
 import { ExcelTable } from './components/ExcelTable';
+import { GalleryReport } from './components/GalleryReport';
 
 function App() {
   return (
@@ -12,8 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Esto es un repaso de React con Johan Tanta</p>
-        <PDFTable />
-        <ExcelTable />
+        <PDFTable/>
+        <ExcelTable/>
+        <GalleryReport/>
       </header>
     </div>
   )
---
+++ src/components/GalleryReport.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const GalleryReport = () => {
+    const execute = () => {
+        
+    }
+
+    return (
+        <div>
+            <button onClick={() => execute()}>Generar reporte galeria</button>
+        </div>
+    )
+}
