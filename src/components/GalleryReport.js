35005dd2289c3778eeda839429ae3eea93ecf074
JohanTv <johan.tanta@utec.edu.pe>
2022-11-19 00:16:00 -0500

    first page completed, galleryreport

--- src/components/GalleryReport.js
+++ src/components/GalleryReport.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import jsPDF from 'jspdf'
 
 export const GalleryReport = () => {
     const execute = () => {
+
+        const exportGalleryFormatToPDF = () => {
+            const filename = "obras_galeria.pdf"
+            const format = "a4"
+            const orientation = "portrait"
+            const options = {
+                orientation: orientation,
+                unit: "px",
+                format: format
+            }
+            const doc = new jsPDF(options)
+            
+            // First Page
+            const width = doc.internal.pageSize.width
+            const height = doc.internal.pageSize.height
+            const x = width / 2, y = height / 2
+            doc.setFont("helvetica", "bold")
+            doc.setFontSize(24)
+            doc.text("Obras de arte", x, y)
+            doc.setFont("helvetica", "bolditalic")
+            doc.text("Galeria", x, y + doc.getTextDimensions("A").h)
+            
+            // Artwork pages
+            // doc.addPage(format, orientation)
+
+            doc.save(filename)
+        }
+
+        exportGalleryFormatToPDF()
+        // doc.rect(x, y - dim.h + 1, dim.w + 1, dim.h)
+
         
     }
 
