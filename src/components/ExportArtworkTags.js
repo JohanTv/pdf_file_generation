ab7380390428d2e8db68b9e55e566ce95cdfad4d
JohanTv <johan.tanta@utec.edu.pe>
2022-11-20 03:54:36 -0500

    missing font size definition with regard tag orientation

--- src/components/ExportArtworkTags.js
+++ src/components/ExportArtworkTags.js
@@ -12,14 +12,15 @@ export const ExportArtworkTags = () => {
             images.push(testImage2)
         }
 
-        const fitsProperly = (doc, tagWidth, tagHeight, rows, columns) => {
+        const fitsProperly = (doc, tagWidth, tagHeight, rows, columns, rowSpacing, columnSpacing) => {
             const pageWidth = doc.internal.pageSize.width
             const pageHeight = doc.internal.pageSize.height
 
-            return (pageWidth >= tagWidth * columns) && (pageHeight >= tagHeight * rows)
+            return (pageWidth >= tagWidth * columns + columnSpacing * (columns - 1)) &&
+                (pageHeight >= tagHeight * rows + rowSpacing * (rows - 1))
         }
 
-        const exportTags = (data, images, configuration) => {
+        const exportTags = (data, images, configuration, filename = "etiquetas.pdf") => {
             if (typeof (configuration) !== 'object') 
                 throw new Error("Parameter invalid")
             /*
@@ -29,7 +30,16 @@ export const ExportArtworkTags = () => {
             */
 
             const {width: tagWidth, height: tagHeight, rows, columns} = configuration
-            const filename = "etiquetas.pdf"
+            const drawImageRectangle = configuration.drawImageRectangle || Boolean(false)
+            const align = configuration.align || "left"
+            const font = configuration.font || "helvetica"
+            const rowSpacing = configuration.rowSpacing || 0.5
+            const columnSpacing = configuration.columnSpacing || 0.5
+            const tagOrientation = configuration.tagOrientation || "horizontal"
+
+            if(!["center", "left", "right"].includes(align))
+                throw new Error("align invalid")
+            
             const orientation = "portrait", format = "a4"
             const options = {
                 orientation: orientation,
@@ -39,23 +49,23 @@ export const ExportArtworkTags = () => {
             }
             const doc = new jsPDF(options)
 
-            if (!fitsProperly(doc, tagWidth, tagHeight, rows, columns)) 
+            if (!fitsProperly(doc, tagWidth, tagHeight, rows, columns, rowSpacing, columnSpacing)) 
                 throw new Error("does not fit properly")
 
-            // Draw rectangles
             const pageWidth = doc.internal.pageSize.width
             const pageHeight = doc.internal.pageSize.height
-            const rowSpacing = 0.5, columnSpacing = 0.5
+            
             const pageMarginX = (pageWidth - (tagWidth * columns + columnSpacing * (columns - 1))) / 2
             const pageMarginY = (pageHeight - (tagHeight * rows + rowSpacing * (rows - 1))) / 2
             
-            let idx = 0
-            const font = "helvetica"
-            doc.setFont(font, "normal")
-            const fontFactor = doc.getFontSize() / doc.getTextDimensions("A").h
+            doc.setFont(font, "bold")
+            const fontHeightFactor = doc.getFontSize() / doc.getTextDimensions("A").h
+            const fontFactor = fontHeightFactor
 
+            let idx = 0
             for(let i = 0; i < rows; i++) {
                 for(let j = 0; j < columns; j++) {
+                    doc.setFont(font, "bold")
                     // Tag rectangle
                     const x = pageMarginX + j * (tagWidth + columnSpacing)
                     const y = pageMarginY + i * (tagHeight + rowSpacing)
@@ -73,24 +83,71 @@ export const ExportArtworkTags = () => {
                     const imageWidth = imageProperties.width * ratio * imageFactor;
                     const imageHeight = imageProperties.height * ratio * imageFactor;
 
-                    const tagMarginX = (tagWidth - imageWidth) * 0.25;
-                    const tagMarginY = (tagHeight - imageHeight) * 0.5;
+                    let tagMarginX, tagMarginY
+                    if (tagOrientation === "horizontal") {
+                        tagMarginX = (tagWidth - imageWidth) * 0.25;
+                        tagMarginY = (tagHeight - imageHeight) * 0.5;
+                    } else {
+                        tagMarginX = (tagWidth - imageWidth) * 0.5;
+                        tagMarginY = (tagHeight - imageHeight) * 0.5;
+                    }
 
                     doc.addImage(image, imageProperties.fileType, x + tagMarginX, y + tagMarginY, imageWidth, imageHeight)
-                    doc.rect(x + tagMarginX, y + tagMarginY, imageWidth, imageHeight, "S")
+                    if(drawImageRectangle)
+                        doc.rect(x + tagMarginX, y + tagMarginY, imageWidth, imageHeight, "S")
+
+                    let textMarginX, textMarginY
+                    if (tagOrientation === "horizontal") {
+                        let factor
+                        if(align === "left") factor = 0.05
+                        else if (align === "center") factor = 0.5
+                        else if(align === "right") factor = 0.95
+                        textMarginX = (tagWidth - (tagMarginX + imageWidth)) * factor
+                        textMarginY = (tagHeight - tagMarginY) * 0.05
+                    } else {
+                        if(align === "left") textMarginX = (tagWidth - tagMarginX) * 0.05
+                        else if (align === "center") textMarginX = tagWidth * 0.5
+                        else if(align === "right") textMarginX = (tagWidth - tagMarginX) * 0.95
+                        textMarginY = (tagHeight - (tagMarginY + imageHeight)) * 0.05
+                    }
+
+                    // Define fontSize - missing
                     const fontSize = (imageHeight * 0.9) / data[idx].length * fontFactor
                     doc.setFontSize(fontSize)
-
+                    
+                    const spaceWidth = tagWidth - (tagMarginX + imageWidth + 2 * textMarginX)
+                    const widthNeeded = data[idx].reduce((prev, current) => {
+                        let value = null
+                        if (!current) value = 0
+                        else if (typeof (current) === "string") 
+                            value = doc.getTextDimensions(current).w
+                        else if (typeof (current) !== "string") 
+                            value = doc.getTextDimensions(current.toString()).w 
+                        return Math.max(prev, value)
+                    }, 0)
+                    
+                    if(widthNeeded > spaceWidth){
+                        const fontWidthFactor = doc.getFontSize() / widthNeeded
+                        doc.setFontSize(spaceWidth * fontWidthFactor)
+                    }
+                    
                     // Artwork information
-                    // falta manejar el caso cuando el ancho del texto se sobresale
-                    const posx = x + tagMarginX + imageWidth + (tagWidth - (tagMarginX + imageWidth)) * 0.125
-                    const posy = y + tagMarginY
+                    let posx, posy
+                    if (tagOrientation === "horizontal") {
+                        posx = x + tagMarginX + imageWidth + textMarginX
+                        posy = y + tagMarginY + textMarginY
+                    } else {
+                        posx = x + tagMarginX + textMarginX
+                        posy = y + tagMarginY + imageHeight + textMarginY
+                    }
+                    
                     for(let info = 0; info < data[idx].length; info++){
                         if (info === 0) doc.setFont(font, "bold")
                         else if (info === 1) doc.setFont(font, "italic")
                         else doc.setFont(font, "normal")
-                        doc.text(data[idx][info], posx, posy + (info + 1) * doc.getTextDimensions("A").h, { align: "left" })
+                        doc.text(data[idx][info], posx, posy + (info + 1) * doc.getTextDimensions("A").h, { align: align })
                     }
+
                     idx++
                 }
             }
@@ -118,6 +175,7 @@ export const ExportArtworkTags = () => {
         const data = getArtworksData(artworks, keysForGalleryFormat)
 
         exportTags(data, images, formats.a4.num_14)
+        exportTags(data, images, formats.a4.num_22)
     }
     
     return (
